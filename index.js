e4cd311a223218fa33755b2eebbd42550b63525e
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-24 09:12:21 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+//code!
+const employee = {
+    name:'Erick', 
+    streetAddress:'51 Lukenya'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key]=value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = employee;
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee=employee;
+    delete newEmployee[key];
+    return newEmployee;
+}
+
